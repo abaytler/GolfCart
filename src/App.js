760d3b1b6c69497b14ca81c3cca8abd6a6c783c6
Alex <asimak4@gmail.com>
2021-08-14 10:19:53 -0400

    first push with carts testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import pic from './pic.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 39.126499,
+      lng: -77.000748
+    },
+    zoom: 15
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '75vh', width: '85%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDAOaA61-Ik4j5wJibuRK_PEzREvyEpZ4w"}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <img
+            src= {pic}
+            className="photo2"
+            alt={"test"}
+            lat={39.126499}
+            lng={-77.000748}
+          />
+          <img
+            src= {pic}
+            className="photo2"
+            alt={"test"}
+            lat={39.123493}
+            lng={-77.000748}
+          />
+          <img
+            src= {pic}
+            className="photo2"
+            alt={"test"}
+            lat={39.126493}
+            lng={-77.010748}
+          />
+          <img
+            src= {pic}
+            className="photo2"
+            alt={"test"}
+            lat={39.122493}
+            lng={-77.010748}
+          />
+          <img
+            src= {pic}
+            className="photo2"
+            alt={"test"}
+            lat={39.213808}
+            lng={-76.798169}
+          />          
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default SimpleMap;
