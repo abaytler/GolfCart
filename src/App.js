083c64ff35ec97cc25fa7197590ed076889f1efc
Andrew Baytler <abaytler@gmail.com>
2021-09-17 13:53:23 -0400

    combined code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import pic from './pic.png';
 import ButtonAppBar from './toolbar.js';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
+const client = new W3CWebSocket('wss://2g54sjwc90.execute-api.us-east-1.amazonaws.com/dev/')
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
@@ -28,11 +29,67 @@ class SimpleMap extends Component {
       }
     }
   }
+  getDataFromLambda() {
+    let wsUri = "wss://2g54sjwc90.execute-api.us-east-1.amazonaws.com/dev/";
+    var output;
 
+    function init() {
+        output = document.getElementById("output");
+        testWebSocket();
+    }
+
+    function testWebSocket() {
+        let websocket = new WebSocket(wsUri);
+
+        websocket.onopen = function(evt) {
+          onOpen(evt)
+        };
+
+        websocket.onmessage = function(evt) {
+          onMessage(evt)
+        };
+
+        websocket.onerror = function(evt) {
+          onError(evt)
+        };
+    }
+
+    function onOpen(evt) {
+        writeToScreen("CONNECTED");
+        doSend('{"message": "hey this is a websocket message", "action": "message"}');
+    }
+
+    function onMessage(evt) {
+      let data = evt.data;
+      let seperatedData = data.split(" ");
+      let lat = seperatedData[0];
+      let long = seperatedData[1];
+      let received_at = seperatedData[2];
+        writeToScreen('<span style = "color: blue;">RESPONSE: ' +
+          lat+'</span>');
+    }
+
+    function onError(evt) {
+        writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+    }
+
+    function doSend(message) {
+        writeToScreen("SENT: " + message); testWebSocket.websocket.send(message);
+    }
+
+    function writeToScreen(message) {
+        var pre = document.createElement("p");
+        pre.style.wordWrap = "break-word";
+        pre.innerHTML = message; output.appendChild(pre);
+    }
+
+    window.addEventListener("load", init, false);
+  }
+	  
   render() {
     return (
       // Important! Always set the container height explicitly
-      
+	  
       <div style={{ height: '75vh', width: '85%', margin:"auto" }}>
         <h1 style={{textAlign:'center',color:'black'}}>Golf Cart Dinos</h1>
         <ButtonAppBar></ButtonAppBar>
@@ -46,7 +103,7 @@ class SimpleMap extends Component {
             src= {pic}
             className="photo2"
             alt={"test"}
-            lat={39.121499}
+            lat={36}
             lng={-77.000748}
           />
           <img
